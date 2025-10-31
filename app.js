761ad962942beec847bcf4e333b6341cf713ab2a
AbhishekPalayadan <abhishekpalayadan20@gmail.com>
2025-10-31 15:53:59 +0530

    Initial commit

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const app=express();
 const env=require('dotenv').config()
 const port=process.env.PORT;
 const db=require('./config/db')
+app.set('view engine','ejs')
 
 db();
 
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
+})
+
+app.get('/login',(req,res)=>{
+    res.render('user/login')
+    console.log('dhofhlfhhl')
 })
 
 module.exports=app;
